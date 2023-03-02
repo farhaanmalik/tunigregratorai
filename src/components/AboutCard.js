e75a6fcf3aa5e8d55650ec6579445c9e98661a76
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-03-02 19:59:29 +0530

    Update AboutCard.js

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -4,18 +4,19 @@ const AboutCard = () => {
     return (
         <>
             <div className="row">
-                <div className="box about-box"><div className="box-body">
-                    <div className="box-number">02</div>
-                    <h4>Yield Farming</h4>
-                    <p>Yield Farming  strategy and staking event. You can now stop switching between Pools  to farm for the best yield, and just sit back to watch your asset grow with our Tuni-Ggregator Ai mechanism.</p>
-                </div></div>
-        <div className="box about-box">
+                 <div className="box about-box">
                     <div className="box-body">
                         <div className="box-number">01</div>
                         <h4>Community driven</h4>
                         <p>Building a Product  of DeFi enthusiasts with a Quirky love for community growth and development.</p>
                     </div>
                 </div>
+                <div className="box about-box"><div className="box-body">
+                    <div className="box-number">02</div>
+                      <h4>Yield Farming</h4>
+                      <p>Yield Farming  strategy and staking event. You can now stop switching between Pools  to farm for the best yield, and just sit back to watch your asset grow with our Tuni-Ggregator Ai mechanism.</p>
+                    </div>
+               </div>
                 <div className="box about-box">
                     <div className="box-body">
                         <div className="box-number">03</div>

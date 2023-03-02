d7f05e9f8133e6be2c599b75e9b69901f610936e
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-03-02 19:57:07 +0530

    Update AboutCard.js

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -4,23 +4,23 @@ const AboutCard = () => {
     return (
         <>
             <div className="row">
-                <div className="box about-box">
+                <div className="box about-box"><div className="box-body">
+                    <div className="box-number">02</div>
+                    <h4>Yield Farming</h4>
+                    <p>Yield Farming  strategy and staking event. You can now stop switching between Pools  to farm for the best yield, and just sit back to watch your asset grow with our Tuni-Ggregator Ai mechanism.</p>
+                </div></div>
+        <div className="box about-box">
                     <div className="box-body">
                         <div className="box-number">01</div>
                         <h4>Community driven</h4>
                         <p>Building a Product  of DeFi enthusiasts with a Quirky love for community growth and development.</p>
                     </div>
                 </div>
-                <div className="box about-box"><div className="box-body">
-                    <div className="box-number">02</div>
-                    <h4>Yield Farming</h4>
-                    <p>Yield Farming  strategy and staking event. You can now stop switching between Pools  to farm for the best yield, and just sit back to watch your asset grow with our Tuni-Ggregator Ai mechanism.</p>
-                </div></div>
                 <div className="box about-box">
                     <div className="box-body">
                         <div className="box-number">03</div>
-                        <h4>DEX</h4>
-                        <p>DEX will be built and powered by AI finding the best Route Swap, getting the best, suitable and fast path.</p>
+                        <h4>Ve(3) Permissionless Dex</h4>
+                        <p>Built  on Arbitrum, following the  governance model called the ve(3,3) system introduced into Solidly, by Andre Conje   and powered by AI finding the best Route Swap, getting the best, suitable and fast path</p>
                     </div>
                 </div>
             </div>

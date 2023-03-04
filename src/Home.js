a1ad6ac004ebe7b7a323815008a4b7835a115692
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-03-04 22:48:35 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,9 @@ const Home = () => {
                     <div className="home-data">
                         <h1 className='topData'>We have integrated  earnings strategy on other secure and protocols with huge return.</h1>
                         <p className='paraData'>Yield Protocol allows users to easily  deposit and enjoy huge yield from our yield farming strategy and staking event.</p>
-                        <button className='btn'>LAUNCH APP <span><i className="fa-solid fa-arrow-right-long"></i></span></button>
+                        <a href='https://tunigregrator.vercel.app/' target="_blank">
+                          <button className='btn'>LAUNCH APP <span><i className="fa-solid fa-arrow-right-long"></i></span></button>
+                        </a>
                     </div>
                 </div>
             </div>
